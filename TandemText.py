900ed3fff36a6b12a8fd8584f2b1a4903e2ee78d
sreal19 <sbrnlb@gmail.com>
2015-03-18 15:25:19 -0400

    Version 0.1
    Performs all text process end to end when user enters input folder
    name. Writes all output to working directory

--- TandemText.py
+++ TandemText.py
@@ -1,8 +1,6 @@
-
 __author__ = 'sr-cv'
 '''
 TANDEM 0.05
-Playing Around with NLTK
 Authors: Stephen Real and Christopher Vitale
 Follow CVDH4 and SBReal on Twitter and Github
 DH Praxis 14-15 CUNY Graduate Center
@@ -18,12 +16,26 @@ from pdfminer.layout import LAParams
 from pdfminer.pdfinterp import PDFResourceManager
 from pdfminer.pdfinterp import PDFPageInterpreter
 from pdfminer.pdfpage import PDFPage
-
 from nltk.corpus import PlaintextCorpusReader
+import nltk.data
+from nltk.corpus import stopwords
+from nltk.corpus.reader import WordListCorpusReader
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import RegexpTokenizer
+import csv
+
+#initialize output file open switch
+outputopen = False
 
 
-#function captures location of image files
-def get_input_folder():
+'''***********************************************
+***                                              *
+***     DEFINE ALL THE OCR FUNCTIONS FIRST       *
+***                                              *
+***********************************************'''
+
+
+def get_input_folder():         #Capture location of user's image files
     global good_input
     folder = raw_input('Enter the folder that contains your images: ');
     if os.path.isdir(folder):
@@ -32,28 +44,24 @@ def get_input_folder():
     else:
         print ("Folder does not exist!")
 
-#helper function to OCR a singe file
-def process_image(file):
+def process_image(file):            #helper function to OCR a singe file
     input_image = Image.open(file)
     input_image= input_image.convert('RGB')
     output_data = pytesseract.image_to_string(input_image)
     return output_data
 
-#run the Tesseract OCR engine using the helper function process_image()
-def ocrit(fullpath, file):
+def ocrit(fullpath, file):          #run Tesseract OCR engine using process_image()
     ofile = os.path.splitext(file)[0]
     outfile = outpath + outfolder + '/' + ofile + '.txt'
     temp = open(outfile, 'w')
     temp.write (str(process_image(fullpath)))
     temp.close()
 
-def pdfconvert(fullpath, file, pages=None):
+def pdfconvert(fullpath, file, pages=None):     #Convert PDF to TXT
     if not pages:
         pagenums = set()
     else:
         pagenums = set(pages)
-
-
     output = StringIO()
     manager = PDFResourceManager()
     converter = TextConverter(manager, output, laparams=LAParams())
@@ -69,14 +77,11 @@ def pdfconvert(fullpath, file, pages=None):
     outfile = outpath + outfolder + '/' + ofile + '.txt'
     text = output.getvalue()
     output.close
-
     temp = open(outfile, 'w')
     temp.write (text)
     temp.close()
 
-
-
-def make_corpus_folder(x):
+def make_corpus_folder(x):              #Create a folder for the OCR Output/NLTK input
     outname = 'ocrout_corpus'
     count = 1
     while True:
@@ -86,8 +91,111 @@ def make_corpus_folder(x):
         except(OSError):
             outname = 'ocrout_corpus' + str(count)
             count += 1
-            print x + outname
+            print "could not create folder ", x + outname, "...retrying..."
     return outname
+
+'''***********************************************
+***                                              *
+***     NOW DEFINE ALL THE NLTK FUNCTIONS        *
+***                                              *
+***********************************************'''
+
+def tokenize_file(file):            #tokenize input file, count words, characters, remove stopwords
+    tokenizer = RegexpTokenizer(r'\w+')
+    item_count = 0
+    total_chars = 0
+    word_count = 0
+    wordlist = []
+
+    item_count = 0
+    reader = WordListCorpusReader(corpus_root, file)
+    chunks = reader.words()
+
+    for item in chunks:
+        total_chars += len(chunks[item_count])
+        word_tokens = tokenizer.tokenize(chunks[item_count])
+        word_count += len(word_tokens)
+        item_count += 1
+        for word in word_tokens:
+            wordlist.append(word)
+    stopsout = [word for word in wordlist if word not in english_stops]
+    return wordlist, stopsout, word_count, total_chars
+
+def build_sorted_ascii(wordlist):       #convert to ascii, lowercase and sort
+    i = 0
+    templist = []
+    for word in wordlist:
+        temp = wordlist[i].encode('ascii', 'ignore')
+        templist.append(temp.lower())
+        i += 1
+    sorted_wordlist = sorted(templist)
+    return sorted_wordlist
+
+def build_unique_dictionary(inlist):        #find unique words and count them
+    unique = [inlist[0].lower()]
+    countlist = [1]
+    i = 1
+    o = 0
+
+    for i in range(1, len(inlist)):
+        #print unique[o]
+        if inlist[i].lower() == unique[o]:
+            countlist[o] += 1
+        else:
+            unique.append(inlist[i].lower())
+            countlist.append(1)
+            o += 1
+        i += 1
+    return unique, countlist
+
+def write_first_row(outname):               #write the first row of the main output file
+    global outputopen, file, resultspath
+
+    print outname
+    with open(outname, 'wb') as csvfile:
+            tandemwriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            tandemwriter.writerow(['File','Total Word Count', 'Total Characters', 'Average Word Length',
+                                   'Unique Word Count', 'Count without Stopword'])
+            tandemwriter.writerow([file]+[allcount]+[allchar]+[avg_word_length]+[len(unique_nonstop_words)]+
+                                  [len(nonstops)])
+
+    outputopen = True
+    write_the_lists()
+
+def write_the_rest(outname):                #write subsequent rows of main output file
+    global file
+    with open(outname, 'a') as csvfile:
+            tandemwriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            tandemwriter.writerow([file]+[allcount]+ [allchar]+[avg_word_length]+[len(unique_nonstop_words)]+[len(nonstops)])
+    write_the_lists()
+
+def write_the_lists():                      #write the wordlists
+    #write list of all the words
+    words_csv = resultspath + os.path.splitext(file)[0] + '_allwords.csv'
+    with open(words_csv, 'wb') as csvfile:
+            wlwriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            wlwriter.writerow(['All Words'])
+            for item in ascii_sorted:
+                wlwriter.writerow([item])
+
+    #write a list of unique words with counts other than stop words
+    unique_csv = resultspath + os.path.splitext(file)[0] + '_unique.csv'
+    with open(unique_csv, 'wb') as csvfile:
+            i = 0
+            unwriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            unwriter.writerow(['Unique Non Stop Words', 'Count'])
+            for i in range (0, len(unique_nonstop_words)):
+                unwriter.writerow([unique_nonstop_words[i]]+[nonstop_counts[i]])
+                i += 1
+'''***********************************************
+***                                              *
+***     NOW WE GET TO THE MAIN LOGIC             *
+***                                              *
+***********************************************'''
 
 #capture the folder that contains image files.
 good_input = False
@@ -95,15 +203,13 @@ while good_input == False:
     infolder = get_input_folder()
 
 #create the output folder
-outpath = '/Users/sbr/nltk_data/corpora/'
+outpath = './'
 outfolder = make_corpus_folder(outpath)
 
-#loop through the user's input folder and find image files
-#for testing purposes only work with PNG files now
+#find image files in the folder and convert them to text
 outcount = 1
-print
-print ("processing image files in " + infolder)
-print
+print "\n", "processing image files in " + infolder, "\n"
+
 
 files = [ f for f in os.listdir(infolder) if os.path.isfile(os.path.join(infolder,f)) ]
 for file in files:
@@ -121,39 +227,48 @@ for file in files:
     elif os.path.splitext(file)[1] == '.pdf':
         pdfconvert(fullpath, file)
     else:
-       print
-       print (file + " is not an image file. Skipped... ")
+       print "\n", file + " is not an image file. Skipped... "
 
-'''
-#NLTK processing of files output
-print
-print 'NLTK Step'
-print
+''' Now all image files have been converted. Analyze them with nltk'''
+print "\n", "starting nltk process ", "\n"
 
-nltk_in_path = infolder + outfolder
-nltk_input_files = os.listdir(nltk_in_path)
-print "nltk folder="
-print nltk_in_path
+#container = nltk.data.load('corpora/ocrout_corpus2/BookScanCenter_9.txt',format='raw')
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+english_stops = set(stopwords.words('english'))
+corpus_root = outfolder
 
-doc_text = PlaintextCorpusReader(nltk_in_path, '.*')
+files = [ f for f in os.listdir(corpus_root) if os.path.isfile(os.path.join(corpus_root,f)) ]
+for file in files:
+    if os.path.splitext(file)[1] == '.txt':
+        allwords, nonstops, allcount, allchar = tokenize_file(file)
+        avg_word_length = round(float(allchar)/float(allcount), 2)
+        ascii_sorted = []
+        ascii_sorted = build_sorted_ascii(allwords)
+        nonstop_sorted = []
+        nonstop_sorted = build_sorted_ascii(nonstops)
+        unique_nonstop_words, nonstop_counts  = build_unique_dictionary(nonstop_sorted)
 
-print "Word Length"
-print
-wl_list = []
-for file in nltk_input_files:
-    nltk_data = [len(doc_text.words(file))]
-    print nltk_data
-    wl_list.append(nltk_data)
+        #write the results of nltk process to csv files
+        resultspath = './'
+        outfile = resultspath + 'tandem' + 'main.csv'
+        if outputopen:
+            write_the_rest(outfile)
+        else:
+            write_first_row(outfile)
 
-print wl_list
-'''
-'''
-out = csv.writer(open('TANDEM_output.csv','w'), delimiter=',',quoting=csv.QUOTE_ALL)
-out.writerow('w')
-out.writerow(data1)
-out.writerow(data2)
-out.writerow(data3)
-out.writerow(data4)
-out.writerow(data5)
-'''
+def merge_all():
+    global resultspath
+
+    allfile = resultspath + 'tandem' + '_bigtext.txt'
+    with open(allfile, 'w') as outfile:
+        for file in files:
+            fullname = corpus_root + '/'+ file
+            if os.path.splitext(file)[1] == '.txt':
+                with open(fullname) as infile:
+                    outfile.write(infile.read())
+            else:
+                print "skipping"
+
+
+
 
